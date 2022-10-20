ce85305cc137758028f8b12a125db9582b774205
Tahsin <tahsin.anam@tietoevry.com>
2022-10-20 14:47:40 +0300

    11 Using RestTemplate to call an external microservice API

---
+++ book-catalog-service/src/main/java/tahsin/spring/bookcatalogservice/Book.java
@@ -0,0 +1,30 @@
+package tahsin.spring.bookcatalogservice;
+
+public class Book {
+    private String bookId;
+    private String name;
+
+    public Book(String bookId, String name) {
+        this.bookId = bookId;
+        this.name = name;
+    }
+
+    public Book() {
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- book-catalog-service/src/main/java/tahsin/spring/bookcatalogservice/BookCatalogResource.java
+++ book-catalog-service/src/main/java/tahsin/spring/bookcatalogservice/BookCatalogResource.java
@@ -3,15 +3,32 @@ package tahsin.spring.bookcatalogservice;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class BookCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(new CatalogItem("all quiet on the western front", "Book on First World War", 5));
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("2345", 5)
+        );
+
+        return ratings.stream().map(rating -> {
+            Book book = restTemplate.getForObject("http://localhost:8082/books/" + rating.getBookId(), Book.class);
+            return new CatalogItem(book.getName(), "It is a good book", rating.getRating());
+        }).collect(Collectors.toList());
+
+//        return Collections.singletonList(new CatalogItem("all quiet on the western front", "Book on First World War", 5));
     }
 }
