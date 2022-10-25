b3920b1c632c82befd1fb975648d066fbce52143
Tahsin <tahsin.anam@tietoevry.com>
2022-10-25 15:01:18 +0300

    13 Using WebClient to make API calls

--- book-catalog-service/src/main/java/tahsin/spring/bookcatalogservice/BookCatalogResource.java
+++ book-catalog-service/src/main/java/tahsin/spring/bookcatalogservice/BookCatalogResource.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,10 +21,12 @@ public class BookCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
 
 
         List<Rating> ratings = Arrays.asList(
@@ -31,7 +35,17 @@ public class BookCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
-            Book book = restTemplate.getForObject("http://localhost:8082/books/" + rating.getBookId(), Book.class);
+//            Book book = restTemplate.getForObject("http://localhost:8082/books/" + rating.getBookId(), Book.class);
+
+            Book book =  webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/books/" + rating.getBookId(), Book.class)
+                    .retrieve()
+                    .bodyToMono(
+                            Book.class
+                    )
+                    .block();
+
             return new CatalogItem(book.getName(), "It is a good book", rating.getRating());
         }).collect(Collectors.toList());
 
